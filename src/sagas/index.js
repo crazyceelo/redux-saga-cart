1bd3dc8404e3dc45f614462ed176e60ceda0cd61
Carlos Perez <solracias@gmail.com>
2018-01-17 19:52:46 -0800

    created the shippingSaga, which auto updates the price for any quantity

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -14,3 +14,4 @@ export { fetchCartSaga } from './fetchCartSaga';
 export { itemDetailsSaga } from './itemDetailsSaga';
 export { itemQuantitySaga } from './itemQuantitySaga';
 export { customerServiceAvailabilitySaga } from './customerServiceAvailabilitySaga';
+export { shippingSaga } from './shippingSaga';
